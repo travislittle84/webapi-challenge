48fa9e0f5cc5793299423df1c2cd621a6a6a0af1
Travis Little <kingtrav@gmail.com>
2019-08-18 17:46:46 -0600

    GETs on Project router completed

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -3,4 +3,16 @@ const Actions = require('../data/helpers/actionModel')
 
 const router = express.Router()
 
+
+// GET all ACTIONS
+
+
+// POST a new ACTION
+
+
+// UPDATE (PUT) an ACTION
+
+
+// DELETE an ACTION
+
 module.exports = router
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -3,4 +3,50 @@ const Projects = require('../data/helpers/projectModel')
 
 const router = express.Router()
 
+
+// GET all PROJECTS
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Projects.get()
+        res.status(200).json(projects)
+        
+    } catch (err) {
+        console.log("ERROR", error)
+        res.status(500).json({ message: "There was an error getting projects from the server"} )
+    }
+    
+})
+
+// GET ONE PROJECT
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const project = await Projects.get(id)
+        res.status(200).json(project)
+    } catch (err) {
+        console.log("ERROR", error)
+        res.status(500).json({ message: "There was an error getting project from the server"} )
+    }
+})
+
+// GET Project ACTIONS
+router.get('/:id/actions', async (req, res) => {
+    try {
+        const { id } = req.params
+        const actions = await Projects.getProjectActions(id)
+        res.status(200).json(actions)
+    } catch (err) {
+        console.log("ERROR", error)
+        res.status(500).json({ message: "There was an error getting ACTIONS for this project"} )
+    }
+})
+
+// POST a new PROJECT
+
+
+// UPDATE (PUT) a PROJECT
+
+
+// DELETE a PROJECT
+
 module.exports = router
