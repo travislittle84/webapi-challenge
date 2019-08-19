e1174dc7f53d7ae59cb27693c35903739a53f45a
Travis Little <kingtrav@gmail.com>
2019-08-18 18:51:49 -0600

    helmet and morgan implemented

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express')
 
+const logger = require('morgan')
+const helmet = require('helmet')
+
 const ProjectsRouter = require('./projects/projects-router.js')
 const ActionsRouter = require('./actions/actions-router.js')
 const server = express()
+
 const bodyParser = express.json()
 server.use(express.json())
+
+server.use(logger('dev'))
+server.use(helmet())
 
 server.use('/api/projects', ProjectsRouter)
 server.use('/api/actions', ActionsRouter)
 
-server.use(bodyParser)
 
-// const helmet = require('helmet')
-// server.use(helmet())
+
+
+server.use(bodyParser)
 
 server.get('/', (req, res) => {
     res.send(`Web Api Sprint Challenge - Travis Little`)
